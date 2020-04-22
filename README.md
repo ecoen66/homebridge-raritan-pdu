e6a0f55ac09c066c785a6f59ae49a92782afce58
ecoen66 <ecoen@yahoo.com>
2020-04-22 16:46:18 -0500

    First try

---
+++ README.md
@@ -0,0 +1,53 @@
+# homebridge-raritan-pdu
+An [Raritan](https://www.raritan.com) PX-5475 PDU plugin for
+[Homebridge](https://github.com/nfarina/homebridge).
+
+# Installation
+Run these commands:
+
+    % sudo npm install -g homebridge
+    % sudo npm install -g homebridge-raritan-pdu
+
+
+NB: If you install homebridge like this:
+
+    sudo npm install -g --unsafe-perm homebridge
+
+Then all subsequent installations must be like this:
+
+    sudo npm install -g --unsafe-perm homebridge-raritan-pdu
+
+# Configuration
+If you're already running `homebridge` on your system,
+then you already have a `~/.homebridge/config.json` file and no configuration is needed!
+
+This is a "dynamic" platform plugin,
+so it will automatically look for SNMP agents on the local network that respond to SNMPv2/public.
+Future versions may allow you to specify addressing and authentication information for individual agents.
+
+If this is your first time with `homebridge`,
+this will suffice:
+
+	{
+		"bridge":
+			{ "name": "Homebridge",
+				"username": "CC:22:3D:E3:CE:30",
+				"port": 51826,
+				"pin": "031-45-154"
+      		},
+		"description": "",
+		"accessories": [
+        	{
+            	"accessory": "Raritan PDU",
+            	"name": "Lab PDU",
+            	"ip": "192.168.1.70",
+            	"snmp_community": "RW"
+        	}
+      	]
+    }
+
+# Supported Agents
+The only tested Raritan PDU model for this plugin is the one that I have in my lab,
+the PX-5475.
+This is accomplished using the [PX-PDU-MIB](https://d3b2us605ptvk2.cloudfront.net/download/PX/v1.5.13/PX-1.5.13-MIB.txt).
+
---
+++ index.js
@@ -0,0 +1,107 @@
+"use strict";
+const {promisify} = require("es6-promisify");
+var snmp = require("net-snmp");
+
+var Characteristic, Service;
+
+module.exports = function(homebridge) {
+	Service = homebridge.hap.Service;
+	Characteristic = homebridge.hap.Characteristic;
+
+	homebridge.registerAccessory("homebridge-raritan-pdu", "Raritan PDU", PDUAccessory);
+}
+
+// For a Raritan 24-port PDU model PX-5475
+
+class PDUAccessory {
+
+	constructor(log, config) {
+		this.log = log;
+		this.services = [];
+		for (var i = 0; i < 24; i++) {
+			var service = new Service.Outlet(`Outlet ${i}`, i);
+			this.services.push(service);
+
+			service.getCharacteristic(Characteristic.On)
+				.on('get', this.getOn.bind(this, i))
+				.on('set', this.setOn.bind(this, i));
+		}
+
+		this.snmp = snmp.createSession(config.ip, config.snmp_community);
+		this.snmp_get = promisify(this.snmp.get.bind(this.snmp));
+		this.snmp_set = promisify(this.snmp.set.bind(this.snmp));
+
+		var outlet_oids = [];
+		for (var i = 0; i < 24; i++) {
+			outlet_oids.push(`1.3.6.1.4.1.13742.4.1.2.2.1.2.${i + 1}`);
+		}
+		var promises = [];
+		for (var i = 0; i < outlet_oids.length; i += 2) {
+			var slice = outlet_oids.slice(i, i + 2);
+			promises.push(this.snmp_get(slice))
+		}
+		Promise.all(promises)
+			.then(results => {
+				var names = results
+					.reduce((prev, current) => {
+						return prev.concat(current);
+					}, [])
+					.map(varbind => {
+						return varbind.value.toString().split(",")[0];
+					});
+				for (var i = 0; i < names.length; i++) {
+					var name = names[i]
+					service = this.services[i];
+					service.displayName = name;
+					service.setCharacteristic(Characteristic.Name, name);
+				}
+				this.log.info('Successfully loaded outlet names: ', names.join(', '));
+			})
+			.catch(error => {
+				this.log.error(error.stack);
+			});
+	}
+
+	getServices() {
+		return this.services;
+	}
+
+	getOn(index, callback) {
+		this.log.info(`Retrieving socket ${index}.`);
+		var switch_oids = [];
+		switch_oids.push(`1.3.6.1.4.1.13742.4.1.2.2.1.3.${index + 1}`);
+		this.snmp_get(switch_oids)
+			.then(varbinds => {
+				var on = varbinds[0].value == 1
+				this.log.info(`Socket ${index} is ${on}.`);
+				callback(null, on);
+			})
+			.catch(error => {
+				this.log.info(`Error retrieving socket ${index} status.`);
+				callback(error, null);
+			});
+	}
+
+	setOn(index, on, callback) {
+		this.log.info(`Switching socket ${index} to ${on}.`);    
+		var switch_oid = `1.3.6.1.4.1.13742.4.1.2.2.1.3.${index + 1}`;
+		var toggle = on ? 1 : 0;
+		var snmp_parms = [
+					{
+						oid: switch_oid,
+						type: snmp.ObjectType.Integer,
+						value: toggle
+					}
+				];
+		this.snmp_set(snmp_parms)
+			.then(() => {
+				this.log.info(`Successfully switched socket ${index} to ${on}.`);
+				callback(null);
+			})
+			.catch(error => {
+				this.log.error(`Error switching socket ${index} to ${on}.`);
+				callback(error);
+			});
+	}
+
+}
